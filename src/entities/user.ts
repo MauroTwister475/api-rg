ded7f3eb881f982ab57eb77eaa2b8141850c808b
Mauro Dinis Raimundo <140101450+MauroTwister475@users.noreply.github.com>
2024-03-08 19:05:20 +0100

    Update user.ts

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -1,5 +1,4 @@
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
-import { IsEmail, IsStrongPassword, MaxLength, MinLength } from 'class-validator'
 
 @Entity('user')
 export class User{
@@ -7,17 +6,11 @@ export class User{
     id: string
 
     @Column({type: 'text'})
-    @MaxLength(50, { message: 'Nome deve possuir no máximo 50 caracteres'})
-    @MinLength(8, { message: 'Nome deve possuir no minimo 8 caracteres'})
     name: string
 
     @Column({type: 'text', unique: true})
-    @IsEmail()
     email: string
 
     @Column({type: 'text'})
-    @MaxLength(17, { message: 'A senha deve possuir no máximo 17 caracteres'})
-    @MinLength(8, { message: 'A senha deve possuir no minimo 8 caracteres'})
-    @IsStrongPassword()
     password: string
 }
