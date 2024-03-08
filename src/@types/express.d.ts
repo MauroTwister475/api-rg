e9a6824c426ff9ea54241de2cfda4103d84543fb
Mauro Dinis Raimundo <140101450+MauroTwister475@users.noreply.github.com>
2024-03-08 19:12:09 +0100

    maluquici

--- src/@types/express.d.ts
+++ src/@types/express.d.ts
@@ -1,4 +1,4 @@
-import { User } from '../entities/User'
+import { User } from '../entities/user'
 
 declare global {
 	namespace Express {
--- src/repositories/UserRepository.ts
+++ src/repositories/UserRepository.ts
@@ -1,5 +1,5 @@
 import { AppDataSource } from '../data-source'
-import { User } from '../entities/User'
+import { User } from '../entities/user'
 
 export const UserRepository = AppDataSource.getRepository(User)
 
