bd9a5691f9f12bf97a2c311469429157896e563a
Afonso Da Silva Vicente <afonsodasilva0308@gmail.com>
2024-04-01 16:55:50 +0100

    Update 1711983633871-default.ts

--- src/migrations/1711983633871-default.ts
+++ src/migrations/1711983633871-default.ts
@@ -4,8 +4,8 @@ export class Default1711983633871 implements MigrationInterface {
     name = 'Default1711983633871'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "report" ADD "author" text`);
-        await queryRunner.query(`ALTER TABLE "report" ADD "tendencies" text`);
+        await queryRunner.query(`ALTER TABLE "report" ADD "author" text NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "report" ADD "tendencies" text NOT NULL`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
